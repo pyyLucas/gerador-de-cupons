5fb23ce2368f7fb173c202ec86df349d90b37ab6
Lucas <luccassoaressilva2@gmail.com>
2023-01-25 13:26:41 -0300

    gerador de cupom 1.2
    Este código importa as bibliotecas random, string e csv, usa a função string.ascii_letters e string.digits para gerar uma sequencia de letras e números e a função random.choice() para escolher caracteres aleatórios. A função generate_coupon gera um cupom com comprimento de 8 caracteres (padrão) e a função save_to_csv(coupons) salva os cupons gerados em um arquivo csv chamado "coupons.csv".
    
    Você pode ajustar o comprimento do cupom e o número de cupons gerados de acordo com suas necessidades. E também pode salvar esses cupons gerados numa lista ou num banco de dados.

--- gerador de cupons.py
+++ gerador de cupons.py
@@ -1,10 +1,24 @@
+import random
+import string
+import csv
 
-    from asyncio.windows_events import INFINITE
-base ="www.teste.com"
-c = "cupom/teste"
-d = 50
-g = INFINITE
-for num in range (g):
-    print(f"{base}/{c}/{d}/{num}/")
-    new_var = print (f"{num} de {g} cupons gerados")
-  
+def generate_coupon(length=8):
+    # gerando uma sequencia aleatória de caracteres
+    letters_and_digits = string.ascii_letters + string.digits
+    coupon = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    return coupon
+
+def save_to_csv(coupons):
+    with new_func() as file:
+        writer = csv.writer(file)
+        writer.writerow(["Cupom"])
+        for coupon in coupons:
+            writer.writerow([coupon])
+    print("Cupons salvos com sucesso!")
+
+def new_func():
+    return open('coupons.csv', 'w', newline='')
+
+# gerando 100 cupons
+coupons = [generate_coupon() for i in range(100)]
+save_to_csv(coupons)
