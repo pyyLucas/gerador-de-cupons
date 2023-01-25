d48d9a05ccd4793684939170c0240e49602f094a
Lucas <luccassoaressilva2@gmail.com>
2023-01-25 13:03:56 -0300

    Update gerador de cupons.py

--- gerador de cupons.py
+++ gerador de cupons.py
@@ -1,9 +1,10 @@
 
-from asyncio.windows_events import INFINITE
+    from asyncio.windows_events import INFINITE
 base ="www.teste.com"
 c = "cupom/teste"
 d = 50
 g = INFINITE
 for num in range (g):
-    print(f"{base}/{c}/{d}/{num}")
+    print(f"{base}/{c}/{d}/{num}/")
     new_var = print (f"{num} de {g} cupons gerados")
+  
